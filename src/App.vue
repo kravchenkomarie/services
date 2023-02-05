98d9b8fbb08cdb2cb0744e550bacf682aa98c48f
Maria <kravchenkomarie@gmail.com>
2023-02-06 02:04:44 +0300

    change App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
+  <Home />
   <router-view />
 </template>
 
 <script>
+import Home from "../src/views/Home.vue";
 export default {
   name: "app",
-  components: {},
+  components: {
+    Home,
+  },
   data() {
     return {};
   },
